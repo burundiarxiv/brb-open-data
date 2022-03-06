e4cca8abef784a0f4f917e8c57f8dcce98d7e9cd
duolingo <irabaruta freeman>
2022-03-06 12:47:35 +0200

    Adding the functions for data manipulation

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
+import csv
+from email.header import Header
 from operator import ge
 from bs4 import BeautifulSoup
 import requests
 from urllib.parse import unquote
 import pyexcel as converter
+import openpyxl
+import pandas as pd
+
 
 URL = "https://brb.bi"
 
@@ -23,6 +28,32 @@ def download_x(element):
             open(unquote(link[35:]),'wb').write(data.content)
             converter.save_book_as(file_name=unquote(link[35:]), dest_file_name=unquote(link[35:])+"x")
             print("Successfully Created ",unquote(link[35:]) )
+            return unquote(link[35:])+"x"
 
+def remove_sheets(workbook, sheet_to_keep):
+    workbook_copy = workbook
+    for sheet in workbook.sheetnames:
+        if sheet != sheet_to_keep:
+            del workbook_copy[sheet]
+    return workbook_copy
 
-download_x("Importations_par_rubrique en valeur")
+def remove_n_lines(workbook, start, end):
+    workbook_copy = workbook
+    sheet = workbook_copy["Mensuelle"]
+    sheet.delete_rows(start,end)
+    return workbook_copy
+def xlsx_to_csv(workbook_path):
+    read_file = pd.read_excel(workbook_path)
+    read_file.to_csv("Trial.csv", index=None, header= True)
+
+def create_workbook_from_net(online_notebook):
+    book = openpyxl.load_workbook(download_x(online_notebook))
+    book = remove_sheets(book,"Mensuelle")
+    book = remove_n_lines(book,0,7)
+    book = remove_n_lines(book,99,300)
+    book.save("trial.xlsx")
+    print(book.sheetnames)
+
+    xlsx_to_csv("trial.xlsx")
+
+create_workbook_from_net("Importations_par_rubrique en valeur")
