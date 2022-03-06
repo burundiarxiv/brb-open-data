3edd811c56bfd9098aff767a399e540b5bdbf1cc
duolingo <irabaruta freeman>
2022-03-06 14:49:35 +0200

    fix bug in output directory

--- test.py
+++ test.py
@@ -44,18 +44,19 @@ def remove_n_lines(workbook, start, end): #remove problematic lines
     sheet = workbook_copy["Mensuelle"]
     sheet.delete_rows(start,end)
     return workbook_copy
+
 def xlsx_to_csv(workbook_path):
     read_file = pd.read_excel(workbook_path)
-    read_file.to_csv("Trial.csv", index=None, header= True)
+    read_file.to_csv("csv/Trial.csv", index=None, header= True)
 
 def create_workbook_from_net(online_notebook): #main function, may need changes
     book = openpyxl.load_workbook(download_x(online_notebook))
     book = remove_sheets(book,"Mensuelle")
     book = remove_n_lines(book,0,7)
     book = remove_n_lines(book,99,300)
-    book.save("trial.xlsx")
+    book.save("xls/trial.xlsx")
     print(book.sheetnames)
 
-    xlsx_to_csv("trial.xlsx")
+    xlsx_to_csv("xls/trial.xlsx")
 
 create_workbook_from_net("Importations_par_rubrique en valeur")
