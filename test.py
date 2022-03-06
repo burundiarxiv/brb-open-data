2242450fc50dfd9c6fc948245313c2791c342f26
duolingo <irabaruta freeman>
2022-03-06 14:28:23 +0200

    adding comments for more clarification on the use of different functions

--- test.py
+++ test.py
@@ -9,35 +9,37 @@ import openpyxl
 import pandas as pd
 
 
-URL = "https://brb.bi"
+URL = "https://brb.bi" # The base link 
 
-def get_everything(path):
+#This function is used to gather all the links that we will need. It just needs a path to know the path of the page
+def get_everything(path): 
     page = requests.get(URL + path)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find_all("div",class_= "section")[2] # The second occurence of the section class is what contains our section
-    others = results.find_all("p")
+    others = results.find_all("p") # This contains all the links  
     links = [] 
-    for i in others:
+    for i in others: #Get all the links
         links.append(URL + i.find_all("a")[0]["href"])
-    return links
-def download_x(element):
-    links = get_everything("/fr/content/balance-des-paiements")
-    for link in links:
-        if element in unquote(link):
-            data = requests.get(link)
-            open(unquote(link[35:]),'wb').write(data.content)
-            converter.save_book_as(file_name=unquote(link[35:]), dest_file_name=unquote(link[35:])+"x")
-            print("Successfully Created ",unquote(link[35:]) )
-            return unquote(link[35:])+"x"
+    return links #Then return them
 
-def remove_sheets(workbook, sheet_to_keep):
+def download_x(element): #This function downloads a specified element and converts the xls file to xlsx
+    links = get_everything("/fr/content/balance-des-paiements") #Our path
+    for link in links: 
+        if element in unquote(link):
+            data = requests.get(link) #Get the binary from the page
+            open(unquote(link[35:]),'wb').write(data.content) #Keep the file as it is. And save it on the device. 
+            converter.save_book_as(file_name=unquote(link[35:]), dest_file_name=unquote(link[35:])+"x") # Using a converter to convert the xls to xlsx
+            print("Successfully Created ",unquote(link[35:]), "x")
+            return unquote(link[35:])+"x" #Return the name of the file
+
+def remove_sheets(workbook, sheet_to_keep): #Remove unwanted sheets
     workbook_copy = workbook
     for sheet in workbook.sheetnames:
         if sheet != sheet_to_keep:
             del workbook_copy[sheet]
     return workbook_copy
 
-def remove_n_lines(workbook, start, end):
+def remove_n_lines(workbook, start, end): #remove problematic lines
     workbook_copy = workbook
     sheet = workbook_copy["Mensuelle"]
     sheet.delete_rows(start,end)
@@ -46,7 +48,7 @@ def xlsx_to_csv(workbook_path):
     read_file = pd.read_excel(workbook_path)
     read_file.to_csv("Trial.csv", index=None, header= True)
 
-def create_workbook_from_net(online_notebook):
+def create_workbook_from_net(online_notebook): #main function, may need changes
     book = openpyxl.load_workbook(download_x(online_notebook))
     book = remove_sheets(book,"Mensuelle")
     book = remove_n_lines(book,0,7)
